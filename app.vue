b918b39f98ff0f104c1766ed0fb67aa2caf2d954
Andronick Martusheff <andronick@outlook.com>
2025-06-19 11:21:48 -0700

    Updates

--- app.vue
+++ app.vue
@@ -7,3 +7,15 @@
     </UApp>
   </div>
 </template>
+
+<script setup>
+useHead({
+  meta: [
+    { name: 'color-scheme', content: 'light only' },
+    { name: 'theme-color', content: '#ffffff' },
+    // This is the key one for Android
+    { name: 'mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }
+  ]
+})
+</script>
